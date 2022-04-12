49bf89cb50fab1cc4dc66c32fd4dc0bb3a0cc936
jayson <leobest9688@gmail.com>
2022-04-12 23:27:54 +0800

    add get post test

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,7 +2,6 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -12,10 +11,37 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function test_the_application_returns_a_successful_response()
+    public function test_get_todo_by_id()
     {
-        $response = $this->get('/');
+        $response = $this->get('/todos/1');
 
         $response->assertStatus(200);
     }
+
+    public function test_get_list_Todos()
+    {
+        $response = $this->get('/todos');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_post_add_todo()
+    {
+        $response = $this->postJson('/todos', ['name' => 'Example Todo8', 'description' => 'This is an example Todo', 'due_date' => '04/04/2022', 'is_complete' => 1]);
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'todo' => [
+                    'id' => true,
+                    'name' => true,
+                    'description' => true,
+                    'due_date' => true,
+                    'is_complete' => true],
+
+            ]);
+    }
+
+
+
 }
